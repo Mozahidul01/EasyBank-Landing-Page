177518f4079e38432989d3f4cc29d79e43fe3129
mozop <you@example.com>
2022-08-18 04:10:16 +0600

    responsive navbar created

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log("hello world!")
+// Hamburger Menu Open & Close
+
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function () {
+    
+    if (header.classList.contains('open')) { //Close Hamburger Menu
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
+    }
+
+    else { //Open Hamburger Menu
+        body.classList.add('no-scroll');
+        header.classList.add('open');
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+    }
+})
